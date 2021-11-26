bac4ce399aa62bc6e2d779716b06d39b62c07457
Inmersys <developer@inmersys.com>
2021-11-25 23:52:53 -0600

    se cambio la versio´n

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ export interface NavigationProps {
 
 const Home= () => {
   const classes = useStyles()
-  const version = 'v1.2.0.2'
+  const version = 'v1.2.0.3'
     const router = useRouter();
 
     useEffect(() => {
